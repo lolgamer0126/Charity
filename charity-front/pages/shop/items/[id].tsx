6ad97cf1395a56f232234c3aa471636b277a1817
Ochko <ochkofficial@gmail.com>
2022-08-19 21:48:08 +0800

    navbar focus

---
+++ charity-front/pages/shop/items/[id].tsx
@@ -0,0 +1,150 @@
+import {
+    Box,
+    chakra,
+    Container,
+    Stack,
+    Text,
+    Image,
+    Flex,
+    VStack,
+    Button,
+    Heading,
+    SimpleGrid,
+    StackDivider,
+    useColorModeValue,
+    VisuallyHidden,
+    List,
+    ListItem,
+} from '@chakra-ui/react';
+import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { MdLocalShipping } from 'react-icons/md';
+import SidebarWithHeader from '../../../components/NavbarTwo';
+import Carousel from '../../../components/carousel';
+
+export default function ItemDetail() {
+  return (
+    // <SidebarWithHeader>
+        <Container maxW={'7xl'}>
+          <SimpleGrid
+            columns={{ base: 1, lg: 2 }}
+            spacing={{ base: 8, md: 10 }}
+            py={{ base: 18, md: 24 }}>
+            <Flex>
+                <Carousel/>
+            </Flex>
+            <Stack spacing={{ base: 6, md: 10 }}>
+              <Box as={'header'}>
+                <Heading
+                  lineHeight={1.1}
+                  fontWeight={600}
+                  fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}>
+                  Automatic Watch
+                </Heading>
+                <Text
+                  color={useColorModeValue('gray.900', 'gray.400')}
+                  fontWeight={300}
+                  fontSize={'2xl'}>
+                  ₮350000.00
+                </Text>
+              </Box>
+
+              <Stack
+                spacing={{ base: 4, sm: 6 }}
+                direction={'column'}
+                divider={
+                  <StackDivider
+                    borderColor={useColorModeValue('gray.200', 'gray.600')}
+                  />
+                }>
+                <VStack spacing={{ base: 4, sm: 6 }}>
+              
+                  <Text fontSize={'lg'}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                    aliquid amet at delectus doloribus dolorum expedita hic, ipsum
+                    maxime modi nam officiis porro, quae, quisquam quos
+                    reprehenderit velit? Natus, totam.
+                  </Text>
+                </VStack>
+                <Box>
+                  <Text
+                    fontSize={{ base: '16px', lg: '18px' }}
+                    color={useColorModeValue('yellow.500', 'yellow.300')}
+                    fontWeight={'500'}
+                    textTransform={'uppercase'}
+                    mb={'4'}>
+                    Барааны мэдээлэл
+                  </Text>
+
+                  <List spacing={2}>
+                    <ListItem>
+                      <Text as={'span'} fontWeight={'bold'}>
+                        Шинэ эсэх:
+                      </Text>{' '}
+                      Шинэ
+                    </ListItem>
+                    <ListItem>
+                      <Text as={'span'} fontWeight={'bold'}>
+                        Төрөл:
+                      </Text>{' '}
+                      Гоёлын хэрэгсэл
+                    </ListItem>
+                    <ListItem>
+                      <Text as={'span'} fontWeight={'bold'}>
+                        Материал:
+                      </Text>{' '}
+                      Төмөр
+                    </ListItem>
+                    <ListItem>
+                      <Text as={'span'} fontWeight={'bold'}>
+                        Хэмжээ:
+                      </Text>{' '}
+                      2.5 м
+                    </ListItem>
+                    <ListItem>
+                      <Text as={'span'} fontWeight={'bold'}>
+                        Өнгө:
+                      </Text>{' '}
+                      Хар
+                    </ListItem>
+                    <ListItem>
+                      <Text as={'span'} fontWeight={'bold'}>
+                        Хэдэн ширхэг байгаа:
+                      </Text>{' '}
+                      200
+                    </ListItem>
+                    <ListItem>
+                      <Text as={'span'} fontWeight={'bold'}>
+                        Үйлдвэрлэгдсэн он:
+                      </Text>{' '}
+                      2020.5.1{' '}
+                    </ListItem>
+                  </List>
+                </Box>
+              </Stack>
+
+              <Button
+                rounded={'none'}
+                w={'full'}
+                mt={8}
+                size={'lg'}
+                py={'7'}
+                bg={useColorModeValue('gray.900', 'gray.50')}
+                color={useColorModeValue('white', 'gray.900')}
+                textTransform={'uppercase'}
+                _hover={{
+                  transform: 'translateY(2px)',
+                  boxShadow: 'lg',
+                }}>
+                Захиалах
+              </Button>
+
+              <Stack direction="row" alignItems="center" justifyContent={'center'}>
+                <MdLocalShipping />
+                <Text>Та 5 цагийн дотор ирж бараагаа авна уу</Text>
+              </Stack>
+            </Stack>
+          </SimpleGrid>
+        </Container>
+    // </SidebarWithHeader>
+  );
+}
